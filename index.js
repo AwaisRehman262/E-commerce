80e116b5585d6b8431fc17ce8a2efad07fbb40db
Awais Rehman <126328366+AwaisRehman262@users.noreply.github.com>
2023-05-08 04:12:34 +0500

    our products completed

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 let products =
-    [{ id: 1, name: "Cube 1", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\cube1.jpeg", is_featured: true },
-    { id: 2, name: "Cube 2", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\cube2.jpeg", is_featured: true },
-    { id: 3, name: "Cube 3", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\cube3.jpeg", is_featured: true },
-    { id: 3, name: "Cube 3", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\camera3.jpeg", is_featured: false },
-    { id: 3, name: "Cube 3", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\camera3.jpeg", is_featured: false },
-    { id: 3, name: "Cube 3", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\camera3.jpeg", is_featured: false },
-]
+    [
+        { id: 1, name: "Cube 1", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\cube1.jpeg", is_featured: true },
+        { id: 2, name: "Cube 2", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\cube2.jpeg", is_featured: true },
+        { id: 3, name: "Cube 3", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\cube3.jpeg", is_featured: true },
+        { id: 4, name: "Cube 4", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\camera1.jpeg", is_featured: false },
+        { id: 5, name: "Cube 5", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\camera2.jpeg", is_featured: false },
+        { id: 6, name: "Cube 6", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur porro provident deserunt error ut magnam nam ad molestias quam quod, aliquam ducimus nobis velit, quasi, facere sint rerum omnis. Corrupti quam, amet ", image: "Assets\\camera3.jpeg", is_featured: false },
+    ]
 
 for (let product of products) {
-    if (product.is_featured) {
+    if (product.is_featured == true) {
 
         let mainDiv = document.getElementById('feature_card_wrapper')
+
         let card = document.createElement('DIV')
         mainDiv.appendChild(card)
         card.className = 'feature-card'
@@ -25,44 +27,45 @@ for (let product of products) {
         photo.src = product.image
         card.appendChild(photo)
 
-        let para = document.createElement('P')
-        let para_text = document.createTextNode(product.description)
-        para.appendChild(para_text)
-        card.appendChild(para)
+        let description = document.createElement('P')
+        let description_text = document.createTextNode(product.description)
+        description.appendChild(description_text)
+        card.appendChild(description)
 
         let add_to_cart_button = document.createElement("BUTTON")
         let add_to_cart_button_text = document.createTextNode('add to cart')
         add_to_cart_button.appendChild(add_to_cart_button_text)
-        add_to_cart_button.onclick
+        add_to_cart_button.addEventListener('click', addToCart)
+        add_to_cart_button.id = product.id
         card.appendChild(add_to_cart_button)
+    } else {
+        let ourProductsDiv = document.getElementById('our_product_wrapper')
 
-    } else if (!product.is_featured){
-
-        let mainDiv = document.getElementById('our-products-wrapper')
-        let card = document.createElement('DIV')
-        mainDiv.appendChild(card)
-        card.className = 'feature-card'
+        let ourProductCard = document.createElement('DIV')
+        ourProductsDiv.appendChild(ourProductCard)
+        ourProductCard.className = 'our-product-card'
 
         let heading = document.createElement('H3')
-        card.appendChild(heading)
-
-        let heading_text = document.createTextNode(product.name)
-        heading.appendChild(heading_text)
+        let headingText = document.createTextNode(product.name)
+        heading.appendChild(headingText)
+        ourProductCard.appendChild(heading)
 
         let photo = document.createElement('IMG')
         photo.src = product.image
-        card.appendChild(photo)
+        ourProductCard.appendChild(photo)
 
-        let para = document.createElement('P')
-        let para_text = document.createTextNode(product.description)
-        para.appendChild(para_text)
-        card.appendChild(para)
+        let description = document.createElement('P')
+        let descriptionText = document.createTextNode(product.description)
+        description.appendChild(descriptionText)
+        ourProductCard.appendChild(description)
 
-        let add_to_cart_button = document.createElement("BUTTON")
-        let add_to_cart_button_text = document.createTextNode('add to cart')
-        add_to_cart_button.appendChild(add_to_cart_button_text)
-        object.addEventListener("click", addToCart());
-        card.appendChild(add_to_cart_button)
+        let readMoreButton = document.createElement("BUTTON")
+        let readMoreButtonText = document.createTextNode('see more')
+        readMoreButton.appendChild(readMoreButtonText)
+        ourProductCard.appendChild(readMoreButton)
+        readMoreButton.id = readMoreButton
+        readMoreButton.addEventListener('click',readMore)
+
     }
 }
 
